836135d3da6a44759f35d997d32988374d38080e
Sean Caissie <sirius24@gmail.com>
2011-06-15 14:17:40 -0400

    Fixed typo not showing widget bg

--- SMCNewsController.m
+++ SMCNewsController.m
@@ -31,7 +31,7 @@
     {
         _view = [[UIView alloc] initWithFrame:CGRectMake(2, 0, 316, 71)];
 
-        UIImage *bg = [[UIImage imageWithContentsOfFile:@"/System/Library/WeeAppPlugins/WeeAppTest.bundle/WeeAppBackground.png"] stretchableImageWithLeftCapWidth:5 topCapHeight:71];
+        UIImage *bg = [[UIImage imageWithContentsOfFile:@"/System/Library/WeeAppPlugins/SMCNews.bundle/WeeAppBackground.png"] stretchableImageWithLeftCapWidth:5 topCapHeight:71];
         UIImageView *bgView = [[UIImageView alloc] initWithImage:bg];
         bgView.frame = CGRectMake(0, 0, 316, 71);
         [_view addSubview:bgView];
